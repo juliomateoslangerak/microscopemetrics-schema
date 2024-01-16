f27f009c3edc8bdefa30c49c2eef32a17d24dcda
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-01-16 13:56:08 +0100

    Added a FieldIlluminationDataset examaple and added tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,7 +4,7 @@ import glob
 import unittest
 
 from linkml_runtime.loaders import yaml_loader
-from microscopemetrics_schema.datamodel.microscopemetrics_schema import MetricsDataset
+from microscopemetrics_schema.datamodel.microscopemetrics_schema import MetricsDataset, FieldIlluminationDataset
 
 ROOT = os.path.join(os.path.dirname(__file__), '..')
 DATA_DIR = os.path.join(ROOT, "src", "data", "examples")
@@ -15,8 +15,16 @@ EXAMPLE_FILES = glob.glob(os.path.join(DATA_DIR, '*.yaml'))
 class TestData(unittest.TestCase):
     """Test data and datamodel."""
 
-    def test_data(self):
+    def test_MetricsDataset(self):
         """Data test."""
         for path in EXAMPLE_FILES:
-            obj = yaml_loader.load(path, target_class=MetricsDataset)
-            assert obj
+            if "MetricsDataset" in path:
+                obj = yaml_loader.load(path, target_class=MetricsDataset)
+                assert obj
+
+    def test_FieldIlluminationDataset(self):
+        """Data test."""
+        for path in EXAMPLE_FILES:
+            if "FieldIlluminationDataset" in path:
+                obj = yaml_loader.load(path, target_class=FieldIlluminationDataset)
+                assert obj
