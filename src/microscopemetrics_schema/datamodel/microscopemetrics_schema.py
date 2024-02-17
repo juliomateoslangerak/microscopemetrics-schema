6e7833ac236da7473c278b31ad0aea350375a588
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-02-17 22:17:43 +0100

    Replaced z score by robust z-score

--- src/microscopemetrics_schema/datamodel/microscopemetrics_schema.py
+++ src/microscopemetrics_schema/datamodel/microscopemetrics_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from microscopemetrics_schema.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-02-17T22:08:12
+# Generation date: 2024-02-17T22:11:16
 # Schema: microscopemetrics-schema
 #
 # id: https://w3id.org/MontpellierRessourcesImagerie/microscopemetrics-schema
