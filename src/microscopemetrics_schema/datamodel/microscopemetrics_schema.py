992979a9fbab14bee23494181f8574081fc02038
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2024-10-16 11:20:38 +0200

    Added type and removing manufacturer to sample in FI test dataset

--- src/microscopemetrics_schema/datamodel/microscopemetrics_schema.py
+++ src/microscopemetrics_schema/datamodel/microscopemetrics_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from microscopemetrics_schema.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-10T17:11:06
+# Generation date: 2024-10-16T11:19:34
 # Schema: microscopemetrics-schema
 #
 # id: https://MontpellierRessourcesImagerie.github.io/microscopemetrics-schema
